db5c79046d0b271c9ee3acbae105282b003d1df8
Nicolò Luigi Allegris <77175052+MrTip0@users.noreply.github.com>
2022-03-06 13:25:40 +0100

    made balance a little bit more memory efficent

--- binary/Balance.go
+++ binary/Balance.go
@@ -1,29 +1,62 @@
 package binary
 
 func (b *binaryTree) Balance() {
-	sl := b.ToSlice()
-	b.radice = newNodo(0)
-	b.radice.inserisciBilanciato(sl, 0, len(sl)-1)
+	sl := b.toSliceNodo()
+	b.radice = sl[b.len/2]
+	if b.len == 2 {
+		b.radice.inserisciSx(sl, 0, 0)
+	} else if b.len > 2 {
+		b.radice.inserisciSx(sl, 0, (b.len/2)-1)
+		b.radice.inserisciDx(sl, (b.len/2)+1, b.len-1)
+	}
 }
 
-func (n *nodo) inserisciBilanciato(values []int, min, max int) {
-	nel := max - min
+func (b *binaryTree) toSliceNodo() []*nodo {
+	i := 0
+	r := make([]*nodo, b.len)
+	b.radice.toSliceNodo(r, &i)
+	return r
+}
 
+func (n *nodo) toSliceNodo(s []*nodo, i *int) {
+	if n.sx != nil {
+		n.sx.toSliceNodo(s, i)
+	}
+	n.sx = nil
+	s[*i] = n
+	*i++
+	if n.dx != nil {
+		n.dx.toSliceNodo(s, i)
+	}
+	n.dx = nil
+}
+
+func (n *nodo) inserisciSx(values []*nodo, min, max int) {
+	nel := max - min
 	if nel == 0 {
-		n.val = values[min]
+		n.sx = values[min]
 	} else if nel == 1 {
-		n.val = values[min]
-		n.dx = newNodo(values[max])
-	} else if nel == 2 {
-		n.sx = newNodo(values[min])
-		n.val = values[min+1]
-		n.dx = newNodo(values[max])
+		n.sx = values[max]
+		n.sx.inserisciSx(values, min, min)
 	} else {
-		centro := (max + min) / 2
-		n.val = values[centro]
-		n.sx = newNodo(0)
-		n.sx.inserisciBilanciato(values, min, centro-1)
-		n.dx = newNodo(0)
-		n.dx.inserisciBilanciato(values, centro+1, max)
+		media := (max + min) / 2
+		n.sx = values[media]
+		n.sx.inserisciSx(values, min, media-1)
+		n.sx.inserisciDx(values, media+1, max)
+	}
+}
+
+func (n *nodo) inserisciDx(values []*nodo, min, max int) {
+	nel := max - min
+	if nel == 0 {
+		n.dx = values[min]
+	} else if nel == 1 {
+		n.dx = values[max]
+		n.dx.inserisciSx(values, min, min)
+	} else {
+		media := (max + min) / 2
+		n.dx = values[media]
+		n.dx.inserisciSx(values, min, media-1)
+		n.dx.inserisciDx(values, media+1, max)
 	}
 }
